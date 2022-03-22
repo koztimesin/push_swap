2a251cf13f9145fb7b773da485dc9be96aee9924
Karthus Saffron <a.isumairu01@gmail.com>
2022-03-22 17:13:40 +0300

    added function to count actions for same rotates

--- ft_best_node.c
+++ ft_best_node.c
@@ -6,7 +6,7 @@
 /*   By: ksaffron <ksaffron@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 16:41:25 by ksaffron          #+#    #+#             */
-/*   Updated: 2022/03/22 16:59:21 by ksaffron         ###   ########.fr       */
+/*   Updated: 2022/03/22 17:12:48 by ksaffron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,25 @@ t_list	*best_node(t_list **stack_a, t_list *node)
 		return (temp->next);
 	else
 		return (temp);
+}
+
+static int	actions_sum(t_list **stack_a, t_list **stack_b, t_list *node)
+{
+	int		a_actions;
+	int		b_actions;
+	t_list	*a_node;
+
+	a_node = best_node(stack_a, node);
+	if (rrr_actions(*stack_b, node) < rr_actions(*stack_b, node))
+		b_actions = rrr_actions(*stack_b, node);
+	else
+		b_actions = rr_actions(*stack_b, node);
+	if (b_actions > rrr_actions(*stack_a, a_node)
+		|| b_actions > rr_actions(*stack_a, a_node))
+		return (b_actions);
+	if (rrr_actions(*stack_a, a_node) < rr_actions(*stack_a, a_node))
+		a_actions = rrr_actions(*stack_a, a_node);
+	else
+		a_actions = rr_actions(*stack_a, a_node);
+	return (a_actions + b_actions);
 }

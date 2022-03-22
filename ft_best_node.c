9a19e982729f48711c9c063e08680d8376acc842
Karthus Saffron <a.isumairu01@gmail.com>
2022-03-22 16:50:13 +0300

    recoded

--- ft_best_node.c
+++ ft_best_node.c
@@ -6,8 +6,18 @@
 /*   By: ksaffron <ksaffron@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 16:41:25 by ksaffron          #+#    #+#             */
-/*   Updated: 2022/03/22 16:41:35 by ksaffron         ###   ########.fr       */
+/*   Updated: 2022/03/22 16:45:12 by ksaffron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	rr_actions(t_list *stack, t_list *node)
+{
+	return (ft_lstsize(stack) - ft_lstsize(node));
+}
+
+int	rrr_actions(t_list *stack, t_list *node)
+{
+	return (ft_lstsize(stack));
+}
